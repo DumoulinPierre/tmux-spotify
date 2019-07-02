8fb69adf6de464e45997e6e212e78253178d0a2a
Pierre Dumoulin <dumoulinpierre@icloud.com>
2019-07-02 13:55:38 +0200

    add char limit to album and title

--- scripts/spotify_album.sh
+++ scripts/spotify_album.sh
@@ -8,8 +8,21 @@ source "$CURRENT_DIR/helpers.sh"
 print_spotify_album() {
   connected=$($CURRENT_DIR/spotify_connected.sh 2>&1)
   if [ "$connected" = "OK" ]; then
+    spotify_album_max=$(get_tmux_option "@spotify-album-max" 0)
+
     album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "album"|egrep -v "album"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
-    echo $album
+    album_length=$(expr length "$album")
+
+    if [ $spotify_album_max -eq 0 ]; then
+      echo $album
+    elif [ $spotify_album_max -ge $album_length ]; then
+      echo $album
+    else
+      nb_char=$(($spotify_album_max-3))
+      album_cut=$(echo $album | cut -c -$nb_char)
+      echo -n $album_cut
+      echo "..."
+    fi
   fi
 }
 
--- scripts/spotify_title.sh
+++ scripts/spotify_title.sh
@@ -8,8 +8,21 @@ source "$CURRENT_DIR/helpers.sh"
 print_spotify_title() {
   connected=$($CURRENT_DIR/spotify_connected.sh 2>&1)
   if [ "$connected" = "OK" ]; then
+    spotify_title_max=$(get_tmux_option "@spotify-title-max" 0)
+
     title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
-    echo $title
+    title_length=$(expr length "$title")
+
+    if [ $spotify_title_max -eq 0 ]; then
+      echo $title
+    elif [ $spotify_title_max -ge $title_length ]; then
+      echo $title
+    else
+      nb_char=$(($spotify_title_max-3))
+      title_cut=$(echo $title | cut -c -$nb_char)
+      echo -n $title_cut
+      echo "..."
+    fi
   fi
 }
 
